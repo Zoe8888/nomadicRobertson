f36959bbb3cbaa35370bcd62e36bab3bd80412e0
Jonathan Ngbonga <jongbonga@gmail.com>
2021-08-14 21:27:59 +0200

    init

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'discover',
+        loadChildren: () =>
+          import('../discover/discover.module').then(
+            (m) => m.DiscoverPageModule
+          ),
+      },
+      {
+        path: 'promotions',
+        loadChildren: () =>
+          import('../promotions/promotions.module').then(
+            (m) => m.PromotionsPageModule
+          ),
+      },
+      {
+        path: 'events',
+        loadChildren: () =>
+          import('../events/events.module').then((m) => m.EventsPageModule),
+      },
+      {
+        path: 'liked',
+        loadChildren: () =>
+          import('../liked/liked.module').then((m) => m.LikedPageModule),
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/tab1',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/discover',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+})
+export class TabsPageRoutingModule {}
