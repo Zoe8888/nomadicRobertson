d2934a509fcbb8e61b6a367f41b147c707ebfa1d
Jonathan Ngbonga <jongbonga@gmail.com>
2021-08-18 13:40:01 +0200

    feat(plugin): install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+process.env.TAILWIND_MODE = guessProductionMode() ? "build" : "watch";
+
+module.exports = {
+  prefix: "",
+  mode: "jit",
+  purge: {
+    content: ["./src/**/*.{html,ts,css,scss,sass,less,styl}"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
