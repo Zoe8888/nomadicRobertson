7406b197e13ee86b6369ec1d1d5a576a94ce7b91
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-07 16:21:56 +0200

    feat(env): update base & key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURI: 'https://www.nomadicways.travel/',
+  key: '9839e13a-0ec0-4e30-8115-32b83f6bdb4c',
+  realm: 'ConcourseConnect',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseURI: 'https://www.nomadicways.travel/',
+  key: '9839e13a-0ec0-4e30-8115-32b83f6bdb4c',
+  realm: 'ConcourseConnect',
 };
 
 /*
