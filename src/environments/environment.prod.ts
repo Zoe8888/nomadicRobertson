cb420068fa39a8c4e12929422c428abd5d14b911
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-07 20:52:58 +0200

    feat(env): add open weather key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   baseURI: 'https://www.nomadicways.travel/',
   key: '9839e13a-0ec0-4e30-8115-32b83f6bdb4c',
   realm: 'ConcourseConnect',
+  opweather: '9ea3c24a1b6753e6ec8c0f9d6e7d4ed1',
 };
