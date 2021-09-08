f1cb548ab5a323132b35b53641ec110df722b2dd
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-08 16:08:10 +0200

    feat(tailwind): extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,19 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--ion-color-primary)",
+        secondary: "var(--ion-color-secondary)",
+        tertiary: "var(--ion-color-tertiary)",
+        success: "var(--ion-color-success)",
+        warning: "var(--ion-color-warning)",
+        danger: "var(--ion-color-danger)",
+        dark: "var(--ion-color-dark)",
+        medium: "var(--ion-color-medium)",
+        light: "var(--ion-color-light)",
+      },
+    },
   },
   variants: {
     extend: {},
