0af86967e49aa4efa877ae8d978b86be896f670a
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-25 21:58:05 +0200

    fix(env): update base. removed www

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseURI: 'https://www.nomadicways.travel/',
+  baseURI: 'https://nomadicways.travel/',
   key: '9839e13a-0ec0-4e30-8115-32b83f6bdb4c',
   realm: 'ConcourseConnect',
   opweather: '9ea3c24a1b6753e6ec8c0f9d6e7d4ed1',
