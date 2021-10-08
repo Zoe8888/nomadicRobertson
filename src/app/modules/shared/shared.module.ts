0f03e53797054f7902909f1fd0f99b6d0c4a2d06
Jonathan Ngbonga <jongbonga@gmail.com>
2021-10-09 00:21:56 +0200

    refactor(shared): deleted unused component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { AccordionComponent } from 'src/app/components/accordion/accordion.component';
 import { BlogItemComponent } from 'src/app/components/blog-item/blog-item.component';
-import { DiscoverCardComponent } from 'src/app/components/discover-card/discover-card.component';
 import { DiscoverGridComponent } from 'src/app/components/discover-grid/discover-grid.component';
 import { ProfileDetailsHeaderComponent } from 'src/app/components/profile-details-header/profile-details-header.component';
 import { SwiperModule } from 'swiper/angular';
@@ -9,17 +9,17 @@ import { SwiperModule } from 'swiper/angular';
 @NgModule({
   declarations: [
     BlogItemComponent,
-    DiscoverCardComponent,
     DiscoverGridComponent,
     ProfileDetailsHeaderComponent,
+    AccordionComponent,
   ],
   imports: [CommonModule, SwiperModule],
   exports: [
     BlogItemComponent,
-    DiscoverCardComponent,
     DiscoverGridComponent,
     ProfileDetailsHeaderComponent,
     SwiperModule,
+    AccordionComponent,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
