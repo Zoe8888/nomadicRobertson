8316905d089f1b1bda13bf69088add9e078cd158
Jonathan Ngbonga <jongbonga@gmail.com>
2021-10-09 00:22:23 +0200

    feat(photos): add service

---
+++ src/app/stores/photo/photo.store.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
+import { Photo } from './photo.model';
+
+export interface PhotoState extends EntityState<Photo> {}
+
+@Injectable({ providedIn: 'root' })
+@StoreConfig({
+  name: 'photo',
+  idKey: 'photoId',
+})
+export class PhotoStore extends EntityStore<PhotoState> {
+  constructor() {
+    super();
+  }
+}
