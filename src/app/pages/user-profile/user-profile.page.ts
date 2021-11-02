cf531c65b67b093825970b2f417c0f028350c803
Zoe <zoeerispe@gmail.com>
2021-11-02 12:15:56 +0200

    updated

--- src/app/pages/user-profile/user-profile.page.ts
+++ src/app/pages/user-profile/user-profile.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserQuery, UserService } from 'src/app/stores/user';
 import { LikedService, LikedQuery } from 'src/app/stores/liked';
+import { NavController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user-profile',
@@ -12,20 +14,43 @@ export class UserProfilePage implements OnInit {
   username: any;
   image: any;
   liked: any;
+  fullname: any;
 
   constructor(
     public userQuery: UserQuery,
     public likedService: LikedService,
-    private likedQuery: LikedQuery,
+    public likedQuery: LikedQuery,
     private userService: UserService,
+    private navCtrl: NavController,
+    private modalCtrl: ModalController,
     ) {}
 
     async ngOnInit() {
       this.image = this.userQuery.getValue().data.userImageUrl;
       this.userProfile = this.userQuery.getValue().data;
       this.username = this.userQuery.getValue().username;
-      // this.liked = this.likedService.getList(this.userQuery.getValue().data.userUniqueId.length);
-      // console.log(this.liked)
+      this.fullname = this.userQuery.getValue().data.userName;
+    }
+
+    ionViewWillEnter() {
+      this.likedService.getList();
+      this.liked = this.likedQuery.getValue().ids.length;
+    }
+
+    // likedPofile(){
+    //   const like = this.likedService.getList.length
+    //   this.liked = like.le
+    // }
+
+    dismiss() {
+      this.modalCtrl.dismiss();
+    }
+
+    goTo(profile) {
+      this.navCtrl.navigateForward('business-info', {
+        state: { profile },
+      });
+      this.dismiss();
     }
 
   logout() {
