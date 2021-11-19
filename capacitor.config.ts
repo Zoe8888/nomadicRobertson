4cfdbfd8c151b45a718359bd7556cfe2c13c9062
Sizwe Maluleke <sizwe@mstdevs.co.za>
2021-11-19 06:16:52 +0200

    added/edited missing files for app store connect (IOS)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'za.co.indigo.tulbaghh',
+  appId: 'za.co.indigo.tulbagh',
   appName: 'Tulbagh',
   webDir: 'www',
   bundledWebRuntime: false,
